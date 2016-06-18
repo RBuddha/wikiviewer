40b7388fc83cf9ed1ac23d21af5147b1f18d5577
dar <reclining.buddha@hotmail.com>
2016-06-18 20:30:20 +0000

    bug fixes for lang

--- search.js
+++ search.js
@@ -4,7 +4,12 @@
 $(document).ready(function(){
 
     $('#random').on('click', function(){
-      window.location="https://fr.wikipedia.org/wiki/Special:Random";
+         if($('#french').is(':checked')){
+            window.location="https://fr.wikipedia.org/wiki/Special:Random";
+        }else if($('#english').is(':checked')){
+            window.location="https://en.wikipedia.org/wiki/Special:Random"; 
+            } 
+      
   });
   
   $('#search').click(function(){
@@ -19,7 +24,7 @@ $(document).ready(function(){
   });
   
  var getPages = function(){
-             var value = $('#searchbar').val();
+        var value = $('#searchbar').val();
             if($('#french').is(':checked')){
             var url = "https://fr.wikipedia.org/w/api.php?action=opensearch&search="+value+"&format=json&callback=?";
             }else if($('#english').is(':checked')){
@@ -32,9 +37,23 @@ $(document).ready(function(){
         dataType: 'json',
         success: function(data){
             $('#results').html('');
+            $('#error').html('');  
+            
+            if(data[1].length >= 1){
             for (var i=0; i<data[1].length; i++){
                 $('#results').append("<li><a href="+data[3][i]+" target='_blank'>"+data[1][i]+"</a><p>"+data[2][i]+"</p></li>");
              }
+            }else if(data[1].length === 0){
+                
+                if($('#english').is(':checked')){
+                    $('#error').append('<p class="alert text-center"> nothing found for "'+value+'"</p>');
+                   
+                    
+                }else if($('#french').is(':checked')){
+                    $('#error').append('<p class="alert text-center"> rien de trouvé </p>');
+                    
+                }
+            }
         },
         error: function(errorMessage){
            alert("something went wrong");
